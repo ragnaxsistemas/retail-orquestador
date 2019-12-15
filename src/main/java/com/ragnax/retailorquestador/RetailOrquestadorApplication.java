c045013aab4c1708ee9b042df179a434831346d0
juliocornejo <elimino.gente.por.dinero@gmail.com>
2019-12-15 19:05:07 -0300

    first commit

---
+++ src/main/java/com/ragnax/retailorquestador/RetailOrquestadorApplication.java
@@ -0,0 +1,13 @@
+package com.ragnax.retailorquestador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailOrquestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailOrquestadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnax/retailorquestador/RetailOrquestadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnax.retailorquestador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailOrquestadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
